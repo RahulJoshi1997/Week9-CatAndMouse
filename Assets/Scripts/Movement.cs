df15a94089092491d0bdd4ab75465b1638f93ccd
Rahul Joshi <rahuljoshi@Rahuls-MacBook-Pro.local>
2016-11-01 19:35:32 -0400

    Have the cat and mouse in, as well as the maze. Have just started the Cat and Mouse movements.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	void FixedUpdate () {
+		this.GetComponent<Rigidbody> ().velocity = transform.forward * 10f + Physics.gravity;
+		Ray moveRay = new Ray ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
